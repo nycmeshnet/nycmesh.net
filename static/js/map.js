c01f1f8739d7f8feb7e87911750b233516390c5f
olivernyc <oliver@static.network>
2018-05-24 03:39:09 -0400

    Update map

--- static/js/map.js
+++ static/js/map.js
@@ -1,14 +1,13 @@
 var map;
 var currentNode = {};
-var searchQuery = ""
+var searchQuery = "";
 
 function matchesSearch(query, id) {
-  if (String(id) === String(query))
-    return true
+  if (String(id) === String(query)) return true;
   // var queryIsSupernode = String(query) === "supernode" || String(query) === "Supernode"
   // if (queryIsSupernode && String(id) == String(227))
   //   return true
-  return false
+  return false;
 }
 
 function initMap() {
@@ -36,9 +35,9 @@ function initMap() {
       ]
     },
     {
-      "featureType": "landscape",
-      "elementType": "labels",
-      "stylers": [
+      featureType: "landscape",
+      elementType: "labels",
+      stylers: [
         {
           // "visibility": "off"
         }
@@ -132,14 +131,14 @@ function initMap() {
   var styledMap = new google.maps.StyledMapType(styles, { name: "Styled Map" });
 
   var mapOptions = {
-    center: { lat: 40.7281809, lng: -73.9595798 },
+    center: { lat: 40.7101809, lng: -73.9595798 },
     zoom: 13,
     disableDefaultUI: false,
     zoomControl: true,
     scrollwheel: false,
     streetViewControl: false,
     mapTypeControl: false,
-    backgroundColor: 'none'
+    backgroundColor: "none"
   };
 
   var map = new google.maps.Map(document.getElementById("map-div"), mapOptions);
@@ -156,7 +155,6 @@ function initMap() {
   linksLayer.loadGeoJson("/nodes/links.json");
   linkNYCLayer.loadGeoJson("/nodes/linkNYC.json");
 
-
   //changing icon opacity to show it has panoramas, also set supernode icon
   activeNodesLayer.setStyle(function(feature) {
     var url = "/img/map/active.svg";
@@ -170,7 +168,10 @@ function initMap() {
       //url = '../assets/images/activepano.svg';
       opacity = 1;
     }
-    if (searchQuery.length > 0 && !matchesSearch(searchQuery, feature.getProperty('id'))) {
+    if (
+      searchQuery.length > 0 &&
+      !matchesSearch(searchQuery, feature.getProperty("id"))
+    ) {
       visible = false;
     }
     return {
@@ -198,7 +199,10 @@ function initMap() {
       //url = '../assets/images/potentialpano.svg';
       opacity = 1;
     }
-    if (searchQuery.length > 0 && !matchesSearch(searchQuery, feature.getProperty('id'))) {
+    if (
+      searchQuery.length > 0 &&
+      !matchesSearch(searchQuery, feature.getProperty("id"))
+    ) {
       visible = false;
     }
     return {
@@ -217,17 +221,17 @@ function initMap() {
   linksLayer.setStyle(function(link) {
     var strokeColor = "#ff3b30";
     var opacity = 0.5;
-    var visible = true
+    var visible = true;
     if (link.getProperty("status") != "active") {
       strokeColor = "gray";
       opacity = 0.25;
     }
 
-
     if (searchQuery.length > 0) {
-      var linkMatches = matchesSearch(searchQuery, link.getProperty("from")) || matchesSearch(searchQuery, link.getProperty("to"))
-      if (!linkMatches)
-        visible = false;
+      var linkMatches =
+        matchesSearch(searchQuery, link.getProperty("from")) ||
+        matchesSearch(searchQuery, link.getProperty("to"));
+      if (!linkMatches) visible = false;
     }
 
     return {
@@ -263,41 +267,56 @@ function initMap() {
   activeNodesLayer.setMap(map);
 }
 
-function filterToSearch() {
-
-}
+function filterToSearch() {}
 
 function showImage(panorama) {
-  var lightbox = document.getElementById('lightbox');
+  var lightbox = document.getElementById("lightbox");
   var image = lightbox.children[0];
-  image.src = "./panorama/"+panorama
-  image.classList.remove('dn');
-  lightbox.classList.add('fixed');
-  lightbox.classList.remove('dn');
+  image.src = "./panorama/" + panorama;
+  image.classList.remove("dn");
+  lightbox.classList.add("fixed");
+  lightbox.classList.remove("dn");
 }
 
 function showDetails(event) {
-  var node = event.feature.f
-  currentNode = node
-  var infoWindow = document.getElementById('infoWindow')
-  var statusString = node.status || 'Potential'
-  infoWindow.innerHTML = '<div class="flex items-center justify-start">'+'<h2 class="mv0 near-black f5">Node #'+node.id+'</h2>'+'<p class="mv0 mh2 f6 fw5 dib br4 ph2 pv05 '+statusString+'">'+statusString+'</p>'+'</div>'
+  var node = event.feature.f;
+  currentNode = node;
+  var infoWindow = document.getElementById("infoWindow");
+  var statusString = node.status || "Potential";
+  infoWindow.innerHTML =
+    '<div class="flex items-center justify-start">' +
+    '<h2 class="mv0 near-black f5">Node #' +
+    node.id +
+    "</h2>" +
+    '<p class="mv0 mh2 f6 fw5 dib br4 ph2 pv05 ' +
+    statusString +
+    '">' +
+    statusString +
+    "</p>" +
+    "</div>";
   if (node.notes) {
-    infoWindow.innerHTML += '<p class="f6 fw5 gray">'+node.notes+'</p>'
+    infoWindow.innerHTML += '<p class="f6 fw5 gray">' + node.notes + "</p>";
   }
   if (node.roof) {
-    infoWindow.innerHTML += '<p class="f6 fw5 green">'+'✓ Roof Access'+'</p>'
+    infoWindow.innerHTML +=
+      '<p class="f6 fw5 green">' + "✓ Roof Access" + "</p>";
   }
   if (node.panoramas) {
-
-    String(node.panoramas).split(',').forEach(function(panorama) {
-      infoWindow.innerHTML += '<img class="mt3" onclick="showImage(\''+panorama+'\')" src="./panorama/'+panorama+'"></img>'
-    })
+    String(node.panoramas)
+      .split(",")
+      .forEach(function(panorama) {
+        infoWindow.innerHTML +=
+          '<img class="mt3" onclick="showImage(\'' +
+          panorama +
+          '\')" src="./panorama/' +
+          panorama +
+          '"></img>';
+      });
   }
-  infoWindow.classList.add('db');
+  infoWindow.classList.add("db");
 }
 
 function hideDetails() {
-  var infoWindow = document.getElementById('infoWindow')
-  infoWindow.classList.remove('db');
+  var infoWindow = document.getElementById("infoWindow");
+  infoWindow.classList.remove("db");
 }
