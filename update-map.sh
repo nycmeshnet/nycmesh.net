6e3322d5767b90227bb434ff536e43e0ae417c50
olivernyc <oliver@static.network>
2018-10-04 19:59:34 -0400

    Fix netlify build?

--- update-map.sh
+++ update-map.sh
@@ -3,8 +3,8 @@ set -e
 rm -rf node-map-public/
 git clone https://github.com/olivernyc/node-map-public
 cd node-map-public
-yarn update-data
-yarn
-yarn build
+npm run update-data
+npm install
+npm run build
 cp build/index.html ../layouts/partials/node-map.html
 cp -r build/static/ ../static/map/static/
