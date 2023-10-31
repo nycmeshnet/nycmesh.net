79e62f8ef4f28fec4803b53bd672f10bd0993137
Will Nilges <wdn5796@rit.edu>
2023-10-30 23:12:06 -0400

    Redirect to new status page

---
+++ layouts/shortcodes/redirect_to_status_page.js
@@ -0,0 +1,8 @@
+<div id="status_page_redirect">        
+  <script type="text/javascript">
+  </script>
+  <script>
+		window.location.replace("https://status.mesh.nycmesh.net");
+  </script>
+</div>
+
