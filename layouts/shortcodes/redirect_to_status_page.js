295673027eefd7e3ad21cb4d55f00d3e1e0ca440
Willard Nilges <willardnilges@gmail.com>
2023-11-06 18:06:18 -0500

    Add redirect via netlify

--- layouts/shortcodes/redirect_to_status_page.js
+++
@@ -1,8 +0,0 @@
-<div id="status_page_redirect">        
-  <script type="text/javascript">
-  </script>
-  <script>
-		window.location.replace("https://status.mesh.nycmesh.net");
-  </script>
-</div>
-
